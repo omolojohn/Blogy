90f2f1d8b77eaac976ee048298307d4581ec9c63
omolo-j <omoloj135@gmail.com>
2025-02-24 10:40:09 +0300

    render_tepmlates

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
-from flask import Flask, jsonify, request
+from flask import Flask, render_template, request, url_for
 from server import create_app, db
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy 
+from config import Config
 
-app = create_app()
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+
+
 @app.route("/")
 def home():
-    return jsonify({"message": "Welcome to Blogy API"}), 200
+    return render_template('home.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/post")
+def post():
+    if "user_id" not in session:
+        return redirect(url_for('login'))
+    return render_template('post.html')
+
+
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5555)
+    app.run(debug=True, port=5000)
--- server/models.py
+++ server/models.py
@@ -1,13 +1,15 @@
 from flask_login import UserMixin
 from datetime import datetime
 from server import db
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     phone = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     name = db.Column(db.String(100), nullable=False)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(84), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
     profile_photo = db.Column(db.String(255), default="default.jpg")
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
